306ccff5c5d4895091d4b793d22092edc1177e08
fatinnabilahu <70864114+fatinnabilahu@users.noreply.github.com>
2022-10-16 19:53:58 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/logo-footer1.png" width="200px height="200px" /> <br>
+<font Size="6" face="Poppins">Vocaship</font> <br>
+<font Size="6">Beasiswa Nasional</font>
+</center>
